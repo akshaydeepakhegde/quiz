c3b9cf1f21a1a80b5ef395628082992c0e3d1410
akshaydeepakhegde <hegdeaks@usc.edu>
2019-01-20 02:04:29 -0800

    Initial Commit

---
+++ src/qui/Quiz.java
@@ -0,0 +1,131 @@
+package qui;
+
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Quiz extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Quiz();
+	}
+	JPanel p=new JPanel();
+	CardLayout cards=new CardLayout();
+	int num = 1;
+	boolean v = false;
+	int i=0; 
+
+	int wrongs = 0;
+	int corrects=0;
+	int total=9;
+	
+	String[][] answers={
+		{"True","False"},
+		{"PHP","Python","C++","Java"},
+		{"Adobe Flash Player","Blender","Linux","Android"},
+		{"Lazarus","Premake","make","None of the above"},
+		{"The Open Source Institute","The Open Source Committee","The Open Source Initiative","The Open Source Board"},
+		{"True","False"},
+		{"True","False"},
+		{"Microsoft","HP","Netscape","Apple"},
+		{"Windows 10","Mac OS X","Linux","None of the above"},
+		{"Steve Jobs", "Bill Gates", "Linus Torvalds","Richard Stallman"}
+	};
+	
+	RadioQuestion questions[]={
+		
+		new RadioQuestion(
+			"Open source software is Freely Licensed Software.",
+			answers[0],
+			0,this
+		),
+		new RadioQuestion(
+			"Which language is used to write the most open source packages?",
+			answers[1],
+			3,this
+		),
+		new RadioQuestion(
+			"Which of these is NOT an example of FOSS?",
+			answers[2],
+			0,this
+		),
+
+		new RadioQuestion(
+			"This popular open source build tool ensures that source files that have been updated are compiled into a new version of a program.?",
+			answers[3],
+			2,this
+		),
+		new RadioQuestion(
+			"Which organization defines Open Source rules?",
+			answers[4],
+			1,this
+		),
+		new RadioQuestion(
+			"MySql, Samba and Perl are Open Source software.",
+			answers[5],
+			0,this
+		),
+		new RadioQuestion(
+			"True or False, Open Source software is expensive for its users.",
+			answers[6],
+			1,this
+		),
+		new RadioQuestion(
+			"Which was the first commercial company to put its software in Open Source?",
+			answers[7],
+			2,this
+		),
+		new RadioQuestion(
+			"Which of these is an example of FOSS?",
+			answers[8],
+			2,this
+		),
+		new RadioQuestion(
+			"Who was the founder of the Free Software movement?",
+			answers[9],
+			3,this
+		)
+	};
+	
+	public Quiz(){
+
+		super("FOSS Quiz");
+		setResizable(true);
+		setSize(600,400);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		p.setLayout(cards);
+		num= questions.length;
+		for(int i=0;i<num;i++){
+			p.add(questions[i],"q"+i);
+		}
+		
+		add(p);
+		setVisible(true);
+	}
+	
+	public void next(){
+		if((corrects+wrongs)==total){
+			showSummary();
+		}else{
+ 
+ 			cards.show(p,"q"+i);
+			i = i + 1;
+ 		 
+		}
+	}
+	
+	public void showSummary(){
+		JOptionPane.showMessageDialog(null,"Here are your results"+
+ 			
+ 			"\nScore: \t"+corrects+
+ 			
+ 			"\nTotal: 10"
+		);
+		System.exit(0);
+	}
+
+}
---
+++ src/qui/RadioQuestion.java
@@ -0,0 +1,88 @@
+package qui;
+
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.util.Random;
+import java.awt.CardLayout;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+
+public class RadioQuestion extends JPanel implements ActionListener{
+	int correctAns;
+	Quiz quiz;	
+ 
+ 	boolean count = false;
+	boolean wcount = false;
+	int selected;
+	boolean used = false;
+ 	JPanel qPanel=new JPanel();
+ 	JPanel aPanel=new JPanel();
+	JRadioButton[] responses;
+	ButtonGroup group=new ButtonGroup();
+ 	JPanel botPanel=new JPanel();
+	JButton next=new JButton("Next");
+	JButton finish=new JButton("Finish");
+ 
+	
+	public RadioQuestion(String q, String[] options, int ans, Quiz quiz){
+		this.quiz=quiz;
+		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		correctAns=ans;
+ 		qPanel.add(new JLabel(q));
+		add(qPanel);
+ 		responses=new JRadioButton[options.length];
+		for(int i=0;i<options.length;i++){
+			responses[i]=new JRadioButton(options[i]);
+			responses[i].addActionListener(this);
+			group.add(responses[i]);
+			aPanel.add(responses[i]);
+		}
+		add(aPanel);
+ 		next.addActionListener(this);
+		finish.addActionListener(this);
+		botPanel.add(next);
+		botPanel.add(finish);
+		add(botPanel);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		Object src=e.getSource();
+ 		if(src.equals(next) ){
+			showResult(); 
+ 			quiz.next();
+		}
+ 		if(src.equals(finish)){
+			quiz.showSummary();
+		}
+ 		for(int i=0;i<responses.length;i++){
+			if(src==responses[i]){
+				selected=i;
+			}
+		}
+	}
+	
+	public void showResult(){
+		String text=responses[selected].getText();
+		quiz.total++;
+		if(selected==correctAns && count == false){
+			quiz.corrects++;
+
+			count = true;
+  		}
+  		else if(selected != correctAns && wcount == false)
+  		{
+			quiz.wrongs++;
+			wcount = true;
+		}
+	}
+}
